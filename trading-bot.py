1dcb48407a919924aa0b6b3568709bbe7e5017d2
Paul Bentham <PJBentham@Gmail.com>
2017-05-16 20:45:34 +0100

    Update trading-bot.py
    Used argparse rather then optget - also propose using a csv including all Poloniex currency pairs as options?

--- trading-bot.py
+++ trading-bot.py
@@ -1,14 +1,14 @@
 import time
-import sys, getopt
+import sys
+import argparse
 import datetime
+import csv
 from poloniex import poloniex
 
 def main(argv):
-	period = 10
-	pair = "BTC_XML"
 	prices = []
-	currentMovingAverage = 0;
-	lengthOfMA = 0
+	currentMovingAverage = 0
+	lengthOfMA = 0	
 	startTime = False
 	endTime = False
 	historicalData = False
@@ -16,87 +16,95 @@ def main(argv):
 	typeOfTrade = False
 	dataDate = ""
 	orderNumber = ""
+	dataPoints = []
+	localMax = []
+	currentResistance = 0.018
 
-	try:
-		opts, args = getopt.getopt(argv,"hp:c:n:s:e:",["period=","currency=","points="])
-	except getopt.GetoptError:
-		print 'trading-bot.py -p <period length> -c <currency pair> -n <period of moving average>'
-		sys.exit(2)
+	with open('polopairs.csv', 'rb') as f:
+		reader=csv.reader(f)
+		pairs = list(reader)[0]
 
-	for opt, arg in opts:
-		if opt == '-h':
-			print 'trading-bot.py -p <period length> -c <currency pair> -n <period of moving average>'
-			sys.exit()
-		elif opt in ("-p", "--period"):
-			if (int(arg) in [300,900,1800,7200,14400,86400]):
-				period = arg
-			else:
-				print 'Poloniex requires periods in 300,900,1800,7200,14400, or 86400 second increments'
-				sys.exit(2)
-		elif opt in ("-c", "--currency"):
-			pair = arg
-		elif opt in ("-n", "--points"):
-			lengthOfMA = int(arg)
-		elif opt in ("-s"):
-			startTime = arg
-		elif opt in ("-e"):
-			endTime = arg
+	parser = argparse.ArgumentParser()
+	
+	parser.add_argument("-t", "--timer", 
+						default=300, 
+						type=int, 
+						choices=[10, 300, 900, 1800, 7200, 14400, 86400], 
+						help="Time Period for Poloniex")
+	parser.add_argument("-c", "--currency",
+						type=str,
+						choices=pairs,
+						help="Poloniex Currency Pairing")
+	parser.add_argument("-p", "--points",
+						type=int,
+						default=10,
+						help="Number of Points for Moving Average")
+	parser.add_argument("-s", "--start", 
+						help="Poloniex Start Time (Unixtimestamp)")
+	parser.add_argument("-e", "--end", 
+						help="Poloniex End Time (Unixtimestamp)")
 
+	args=parser.parse_args()
 
+	timer = args.timer
+	pair = args.currency
+	lengthOfMA = args.points
+	startTime = args.start
+	endTime = args.end
 
-	conn = poloniex('key goes here','key goes here')
+	if not len(argv):
+		parser.print_help()
+		sys.exit(1)
 
-	if (startTime):
-		historicalData = conn.api_query("returnChartData",{"currencyPair":pair,"start":startTime,"end":endTime,"period":period})
+	conn = poloniex("Your_Key","Your_Secret")
+
+	if(startTime):
+		historicalData = conn.api_query("returnChartData", {"currencyPair":pair, "start":startTime, "end":endTime, "period":timer})
 
 	while True:
 		if (startTime and historicalData):
 			nextDataPoint = historicalData.pop(0)
 			lastPairPrice = nextDataPoint['weightedAverage']
 			dataDate = datetime.datetime.fromtimestamp(int(nextDataPoint['date'])).strftime('%Y-%m-%d %H:%M:%S')
-		elif(startTime and not historicalData):
-			exit()
-		else:
+		elif (startTime and not historicalData):
+			sys.exit()
+		else:		
 			currentValues = conn.api_query("returnTicker")
 			lastPairPrice = currentValues[pair]["last"]
 			dataDate = datetime.datetime.now()
 
-		if (len(prices) > 0):
+		if len(prices) > 0:
 			currentMovingAverage = sum(prices) / float(len(prices))
 			previousPrice = prices[-1]
-			if (not tradePlaced):
-				if ( (lastPairPrice > currentMovingAverage) and (lastPairPrice < previousPrice) ):
-					print "SELL ORDER"
-					orderNumber = conn.sell(pair,lastPairPrice,.01)
+			if(not tradePlaced):
+				if((lastPairPrice > currentMovingAverage) and (lastPairPrice < previousPrice)):
+					print("SELL ORDER")
 					tradePlaced = True
 					typeOfTrade = "short"
-				elif ( (lastPairPrice < currentMovingAverage) and (lastPairPrice > previousPrice) ):
-					print "BUY ORDER"
-					orderNumber = conn.buy(pair,lastPairPrice,.01)
+				elif((lastPairPrice < currentMovingAverage) and (lastPairPrice > previousPrice)):
+					print("BUY ORDER")
 					tradePlaced = True
 					typeOfTrade = "long"
 			elif (typeOfTrade == "short"):
-				if ( lastPairPrice < currentMovingAverage ):
-					print "EXIT TRADE"
-					conn.cancel(pair,orderNumber)
+				if( lastPairPrice < currentMovingAverage):
+					print("EXIT TRADE")
 					tradePlaced = False
 					typeOfTrade = False
 			elif (typeOfTrade == "long"):
-				if ( lastPairPrice > currentMovingAverage ):
-					print "EXIT TRADE"
-					conn.cancel(pair,orderNumber)
+				if( lastPairPrice > currentMovingAverage):
+					print("EXIT TRADE")
 					tradePlaced = False
 					typeOfTrade = False
 		else:
-			previousPrice = 0
-
-		print "%s Period: %ss %s: %s Moving Average: %s" % (dataDate,period,pair,lastPairPrice,currentMovingAverage)
+			previousPrice = 0		
+		
+		print("{} Period: {}s, {}:{}, Moving Average: {}".format(dataDate, timer, pair, lastPairPrice, currentMovingAverage))	
 
 		prices.append(float(lastPairPrice))
 		prices = prices[-lengthOfMA:]
-		if (not startTime):
-			time.sleep(int(period))
 
+		if(not startTime):
+			time.sleep(timer)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
